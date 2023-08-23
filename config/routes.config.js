49a089f38b73426ea2f140211e8ff3db6f90b2c7
Veronica Galvez <veronicagalvez88@gmail.com>
2023-08-23 17:24:43 +0200

    arreglos

--- config/routes.config.js
+++ config/routes.config.js
@@ -5,4 +5,9 @@ router.get("/", (req, res) => {
     res.render('home')
 });
 
+router.get('/tweets/new', secure.check, tweets.create)
+router.post('/tweets', secure.check, tweets.doCreate)
+router.get('/tweets', tweets.list)
+router.post('/tweets/:id/delete', tweet.delete)
+
 module.exports = router;
